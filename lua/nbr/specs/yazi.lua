646e2fe353bad486e6eda1be9801facb5c4f3f6d
nbr <nikolaus.brunner@protonmail.ch>
2025-02-24 19:10:37 +0100

    feat(specs/yazi): remove backdrop hooks

--- lua/nbr/specs/yazi.lua
+++ lua/nbr/specs/yazi.lua
@@ -1,5 +1,3 @@
-local ui = require("nbr.lib.ui")
-
 -- NOTE: Image Preview does not work: https://yazi-rs.github.io/docs/image-preview/#neovim
 
 ---@type LazySpec
@@ -29,14 +27,5 @@ return {
         yazi_floating_window_winblend = 10,
         floating_window_scaling_factor = 0.6,
         yazi_floating_window_border = "solid",
-        ---@diagnostic disable-next-line: missing-fields
-        hooks = {
-            yazi_opened = function()
-                ui.create_backdrop()
-            end,
-            yazi_closed_successfully = function()
-                ui.close_backdrop()
-            end,
-        },
     },
 }
