f6ee682f01e3847ae8b0b59582fd6ffc41483d18
nbr <nikolaus.brunner@protonmail.ch>
2025-02-26 21:48:53 +0100

    feat: add neovide config

---
+++ lua/nbr/neovide.lua
@@ -0,0 +1,5 @@
+vim.g.neovide_floating_shadow = false
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5
+vim.g.neovide_floating_corner_radius = 0.5
