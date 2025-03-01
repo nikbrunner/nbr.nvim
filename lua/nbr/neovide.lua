a49cd83161ef75db3a3eb2ed25d285c67b3f599c
nbr <nikolaus.brunner@protonmail.ch>
2025-03-01 11:22:51 +0100

    feat(neovide): add opts

--- lua/nbr/neovide.lua
+++ lua/nbr/neovide.lua
@@ -3,3 +3,6 @@ vim.g.neovide_floating_z_height = 10
 vim.g.neovide_light_angle_degrees = 45
 vim.g.neovide_light_radius = 5
 vim.g.neovide_floating_corner_radius = 0.5
+vim.g.neovide_opacity = 0.9
+vim.g.neovide_normal_opacity = 0.9
+vim.g.neovide_window_blurred = true
