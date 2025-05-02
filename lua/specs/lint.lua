027fe535e960bd9a181d136b09dd36748692bee3
nbr <nikolaus.brunner@protonmail.ch>
2025-05-02 09:55:55 +0200

    feat(lsp): add eslint server

--- lua/specs/lint.lua
+++ lua/specs/lint.lua
@@ -8,24 +8,33 @@ return {
         local fname = vim.uri_to_fname(vim.uri_from_bufnr(bufnr))
 
         local eslint_cmd = "eslint"
-        local eslint_configs = { ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json", ".eslintrc.yaml" }
+        local eslint_configs = {
+            ".eslintrc.js",
+            ".eslintrc.cjs",
+            ".eslintrc.json",
+            ".eslintrc.yaml",
+            "eslint.config.mjs",
+            "eslint.config.js",
+            "eslint.config.cjs",
+            "eslint.config.json",
+        }
         local has_eslint_config = vim.fs.find(eslint_configs, { upward = true, path = fname })[1]
 
         local deno_cmd = "deno"
         local deno_configs = { "deno.json", "deno.jsonc" }
         local has_deno_config = vim.fs.find(deno_configs, { upward = true, path = fname })[1]
 
-        if has_eslint_config then
-            vim.notify(string.format("Using %s for linting", eslint_cmd), vim.log.levels.INFO, { title = "Lint" })
-
-            vim.env.ESLINT_D_PPID = vim.fn.getpid()
-            lint.linters_by_ft = {
-                typescriptreact = { eslint_cmd },
-                typescript = { eslint_cmd },
-                javascript = { eslint_cmd },
-                javascriptreact = { eslint_cmd },
-            }
-        end
+        -- if has_eslint_config then
+        --     vim.notify(string.format("Using %s for linting", eslint_cmd), vim.log.levels.INFO, { title = "Lint" })
+        --
+        --     vim.env.ESLINT_D_PPID = vim.fn.getpid()
+        --     lint.linters_by_ft = {
+        --         typescriptreact = { eslint_cmd },
+        --         typescript = { eslint_cmd },
+        --         javascript = { eslint_cmd },
+        --         javascriptreact = { eslint_cmd },
+        --     }
+        -- end
 
         if has_deno_config then
             vim.notify(string.format("Using %s for linting", deno_cmd), vim.log.levels.INFO, { title = "Lint" })
