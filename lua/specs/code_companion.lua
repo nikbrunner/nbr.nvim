aa6e5fe7c0bc1b5361774a706084ef8ad0227901
nbr <nikolaus.brunner@protonmail.ch>
2025-05-08 18:34:40 +0200

    feat(code_companion): enable history extension and add history keymap
    - Activate `codecompanion-history.nvim` and configure `extensions.history`
    - Change history save path to `stdpath("config")`
    - Map `<leader>aih` to open chat history via `CodeCompanionHistory`

--- lua/specs/code_companion.lua
+++ lua/specs/code_companion.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
-        -- "ravitemer/codecompanion-history.nvim",
+        "ravitemer/codecompanion-history.nvim",
     },
     event = "VeryLazy",
     opts = {
@@ -58,27 +58,27 @@ return {
                 adapter = "anthropic_no_reason",
             },
         },
-        -- extensions = {
-        --     history = {
-        --         enabled = true,
-        --         opts = {
-        --             -- Keymap to open history from chat buffer (default: gh)
-        --             keymap = "gh",
-        --             -- Automatically generate titles for new chats
-        --             auto_generate_title = true,
-        --             ---On exiting and entering neovim, loads the last chat on opening chat
-        --             continue_last_chat = false,
-        --             ---When chat is cleared with `gx` delete the chat from history
-        --             delete_on_clearing_chat = false,
-        --             -- Picker interface ("telescope" or "default")
-        --             picker = "snacks",
-        --             ---Enable detailed logging for history extension
-        --             enable_logging = false,
-        --             ---Directory path to save the chats
-        --             dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
-        --         },
-        --     },
-        -- },
+        extensions = {
+            history = {
+                enabled = true,
+                opts = {
+                    -- Keymap to open history from chat buffer (default: gh)
+                    keymap = "gh",
+                    -- Automatically generate titles for new chats
+                    auto_generate_title = true,
+                    ---On exiting and entering neovim, loads the last chat on opening chat
+                    continue_last_chat = false,
+                    ---When chat is cleared with `gx` delete the chat from history
+                    delete_on_clearing_chat = false,
+                    -- Picker interface ("telescope" or "default")
+                    picker = "snacks",
+                    ---Enable detailed logging for history extension
+                    enable_logging = false,
+                    ---Directory path to save the chats
+                    dir_to_save = vim.fn.stdpath("config") .. "/codecompanion-history",
+                },
+            },
+        },
         prompt_library = {
             ["Generate a Commit Message"] = {
                 strategy = "chat",
@@ -177,6 +177,11 @@ return {
             "<leader>ain",
             "<CMD>CodeCompanionChat<CR>",
             desc = "[C]hat",
+        },
+        {
+            "<leader>aih",
+            "<CMD>CodeCompanionHistory<CR>",
+            desc = "[H]istory",
         },
         {
             "<leader>aiC",
