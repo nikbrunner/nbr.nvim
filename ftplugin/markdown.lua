f81ded8f288e41be1c57a1cb65e5014f07080721
nbr <nikolaus.brunner@protonmail.ch>
2025-05-26 09:55:51 +0200

    feat(ftplugin.markdown): linebreak during insert

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,8 +1,11 @@
 vim.opt_local.wrap = true
+vim.opt_local.conceallevel = 2
+vim.opt_local.linebreak = true
+vim.opt_local.textwidth = 80
+vim.opt_local.formatoptions:append("t") -- Auto-wrap text using textwidth
+vim.opt_local.formatoptions:remove("l") -- Allow wrapping of long lines in insert mode
 
 local map = vim.keymap.set
-
-vim.opt_local.conceallevel = 2
 
 map({ "n", "o", "x" }, "j", "gj", {})
 map({ "n", "o", "x" }, "k", "gk", {})
