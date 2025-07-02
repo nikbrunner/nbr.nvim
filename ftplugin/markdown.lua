5d6d4b38d7e561d09078332dab8213874ee7f861
nbr <nikolaus.brunner@protonmail.ch>
2025-07-02 09:42:55 +0200

    fix(markdown): set default textwidth to 0 for no line limit

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -3,7 +3,7 @@ vim.opt_local.linebreak = true
 
 -- Detect printwidth from .prettierrc or .editorconfig, fallback to 80
 local detected_width = require("lib.files").detect_printwidth()
-vim.opt_local.textwidth = detected_width or 80
+vim.opt_local.textwidth = detected_width or 0
 
 vim.opt_local.formatoptions:append("t") -- Auto-wrap text using textwidth
 vim.opt_local.formatoptions:remove("l") -- Allow wrapping of long lines in insert mode
