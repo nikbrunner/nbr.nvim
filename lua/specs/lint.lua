74962392a0efa0fc72c73ecd4e968ad38fdb6176
nbr <nikolaus.brunner@protonmail.ch>
2025-07-15 17:03:32 +0200

    feat(lint): enhance dynamic linter selection for TypeScript/JavaScript files

--- lua/specs/lint.lua
+++ lua/specs/lint.lua
@@ -2,53 +2,83 @@
 return {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
-    config = function(_, opts)
+    config = function()
         local lint = require("lint")
-        local bufnr = vim.api.nvim_get_current_buf()
-        local fname = vim.uri_to_fname(vim.uri_from_bufnr(bufnr))
 
-        local eslint_cmd = "eslint"
-        local eslint_configs = {
-            ".eslintrc.js",
-            ".eslintrc.cjs",
-            ".eslintrc.json",
-            ".eslintrc.yaml",
-            "eslint.config.mjs",
-            "eslint.config.js",
-            "eslint.config.cjs",
-            "eslint.config.json",
-        }
-        local has_eslint_config = vim.fs.find(eslint_configs, { upward = true, path = fname })[1]
+        -- Helper function to determine which linter to use based on config files
+        local function get_linter_for_buffer(bufnr)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            if fname == "" then
+                return nil
+            end
 
-        local deno_cmd = "deno"
-        local deno_configs = { "deno.json", "deno.jsonc" }
-        local has_deno_config = vim.fs.find(deno_configs, { upward = true, path = fname })[1]
+            -- Check for Biome config first (highest priority)
+            local biome_configs = { "biome.json", "biome.jsonc" }
+            local biome_config = vim.fs.find(biome_configs, { upward = true, path = fname })[1]
+            if biome_config then
+                return "biomejs"
+            end
 
-        -- if has_eslint_config then
-        --     vim.notify(string.format("Using %s for linting", eslint_cmd), vim.log.levels.INFO, { title = "Lint" })
-        --
-        --     vim.env.ESLINT_D_PPID = vim.fn.getpid()
-        --     lint.linters_by_ft = {
-        --         typescriptreact = { eslint_cmd },
-        --         typescript = { eslint_cmd },
-        --         javascript = { eslint_cmd },
-        --         javascriptreact = { eslint_cmd },
-        --     }
-        -- end
+            -- Check for Deno config
+            local deno_configs = { "deno.json", "deno.jsonc" }
+            local deno_config = vim.fs.find(deno_configs, { upward = true, path = fname })[1]
+            if deno_config then
+                return "deno"
+            end
 
-        if has_deno_config then
-            vim.notify(string.format("Using %s for linting", deno_cmd), vim.log.levels.INFO, { title = "Lint" })
-            lint.linters_by_ft = {
-                typescriptreact = { deno_cmd },
-                typescript = { deno_cmd },
-                javascript = { deno_cmd },
-                javascriptreact = { deno_cmd },
+            -- Check for ESLint config (lowest priority)
+            local eslint_configs = {
+                ".eslintrc.js",
+                ".eslintrc.cjs",
+                ".eslintrc.json",
+                ".eslintrc.yaml",
+                "eslint.config.mjs",
+                "eslint.config.js",
+                "eslint.config.cjs",
+                "eslint.config.json",
             }
+            local eslint_config = vim.fs.find(eslint_configs, { upward = true, path = fname })[1]
+            if eslint_config then
+                vim.env.ESLINT_D_PPID = vim.fn.getpid()
+                return "eslint"
+            end
+
+            return nil
         end
 
-        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-            group = vim.api.nvim_create_augroup("nbr.nvim_lint_on_save", {}),
+        -- Set up linters by file type dynamically
+        local function setup_linters()
+            local bufnr = vim.api.nvim_get_current_buf()
+            local linter = get_linter_for_buffer(bufnr)
+
+            if linter then
+                local ft = vim.bo[bufnr].filetype
+                local supported_filetypes = {
+                    javascript = true,
+                    javascriptreact = true,
+                    typescript = true,
+                    typescriptreact = true,
+                    json = true,
+                    jsonc = true,
+                }
+
+                if supported_filetypes[ft] then
+                    lint.linters_by_ft[ft] = { linter }
+                    vim.notify(
+                        string.format("Using %s for linting %s files", linter, ft),
+                        vim.log.levels.INFO,
+                        { title = "Lint" }
+                    )
+                end
+            end
+        end
+
+        local lint_group = vim.api.nvim_create_augroup("nbr.nvim_lint", { clear = true })
+
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "BufEnter", "BufNewFile", "BufReadPost" }, {
+            group = lint_group,
             callback = function()
+                setup_linters()
                 pcall(lint.try_lint)
             end,
         })
