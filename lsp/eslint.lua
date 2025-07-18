7f765f380a170f1d136aba5929b29b3975730651
nbr <nikolaus.brunner@protonmail.ch>
2025-07-18 10:43:17 +0200

    fix(lsp): enhance ESLint language server configuration

--- lsp/eslint.lua
+++ lsp/eslint.lua
@@ -1,10 +1,29 @@
 -- Install with: npm i -g vscode-langservers-extracted
 
+local eslint_configs = {
+    ".eslintrc",
+    ".eslintrc.js",
+    ".eslintrc.json",
+    "eslint.config.js",
+    "eslint.config.mjs",
+}
+
 ---@type vim.lsp.Config
 return {
     cmd = { "vscode-eslint-language-server", "--stdio" },
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "graphql" },
-    root_markers = { ".eslintrc", ".eslintrc.js", ".eslintrc.json", "eslint.config.js", "eslint.config.mjs" },
+    root_markers = eslint_configs,
+    root_dir = function(bufnr, cb)
+        local fname = vim.uri_to_fname(vim.uri_from_bufnr(bufnr))
+
+        -- Only start ESLint if we find an ESLint config file
+        local eslint_config = vim.fs.find(eslint_configs, { upward = true, path = fname })[1]
+
+        if eslint_config then
+            cb(vim.fn.fnamemodify(eslint_config, ":h"))
+        end
+        -- If no config found, cb is not called and LSP won't start
+    end,
     -- Using roughly the same defaults as nvim-lspconfig: https://github.com/neovim/nvim-lspconfig/blob/d3ad666b7895f958d088cceb6f6c199672c404fe/lua/lspconfig/configs/eslint.lua#L70
     settings = {
         validate = "on",
